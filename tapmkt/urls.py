9e919cef8e2d47edf18696ce230aac2124b27a28
Debian <bitnami@ip-172-26-5-218.ec2.internal>
2023-06-05 21:26:16 +0000

    first commit

--- tapmkt/urls.py
+++ tapmkt/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
+
 
 
 ]
